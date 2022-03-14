c1fa9f5042da9b8cdeb424b8617cffadf71e7272
Mikboch <mikolaj.bochinski@innotion.pl>
2022-03-14 11:35:27 +0100

    Started function passing mechanism

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,40 +1,20 @@
 import numpy as np
+from math import *
+
 
 def calculate_function_minimum_from_input():
-    function
-    gradient
-    starting_point
-    step
-    reduction_param
-    loop_limit
-    eps
+    variables = input("Enter all funtion variables separated with a comma: ")
+    function_definition = input("Enter the function, which minimum will be calculated: ")
+    function = eval("lambda {0}: {1}".format(variables, function_definition))
+    gradient_definition = input("Enter the function's gradient: ")
+    gradient = eval("lambda {0}: {1}".format(variables, gradient_definition))
+    starting_point = input("Enter the starting point: ")
+    step = input("Enter the step: ")
+    reduction_param = input("Enter the reduction parameter: ")
+    loop_limit = input("Enter the number of loops: ")
+    eps = input("Enter the precision: ")
 
-
-
-    x = starting_point
-    k = 0
-
-    while True:
-        value = function(x)
-        gradient_value = gradient(x)
-        direction = -1 * gradient_value
-
-        x_next = x + np.multiply(step, direction)
-        next_value = function(x_next)
-
-        if next_value >= value:
-            step = step * reduction_param
-            k = k + 1
-            if k > loop_limit:
-                break
-        else:
-            gradient_value_next = gradient(x_next)
-            if np.linalg.norm(gradient_value_next) <= eps:
-                break
-            k = 0
-            x = x_next
-
-    return x
+    return function, gradient, starting_point, step, reduction_param, loop_limit, eps
 
 
 def calculate_function_minimum(function, gradient, starting_point, step, reduction_param, loop_limit, eps):
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 from fun1 import function1, grad1
 from fun2 import function2, grad2
-from gradient_descent import calculate_function_minimum
+from gradient_descent import calculate_function_minimum, calculate_function_minimum_from_input
 
 # for function visualisation: 1.5 - exp(-x^2 - y^2) - 0.5 * exp(-(x-1)^2 - (y+2)^2)
 
@@ -18,31 +18,33 @@ starting_point_array_1 = [(np.random.uniform(0, 1) - 0.5) * 20 for i in range(0,
 starting_point_array_1.sort()
 print(starting_point_array_1)
 
-fig1 = make_subplots(rows=4, cols=1)
+# fig1 = make_subplots(rows=4, cols=1)
+#
+# for i in range(len(step_array_1)):
+#     solutions_array_1 = list(
+#         map(lambda starting_point: calculate_function_minimum(function1, grad1, starting_point, step_array_1[i], 0.9, 15, 0.001),
+#             starting_point_array_1))
+#
+#     df = pd.DataFrame(dict(
+#         x=starting_point_array_1,
+#         y=solutions_array_1
+#     ))
+#
+#
+#     plt.subplot(4,1,i+1)
+#     plt.plot(starting_point_array_1, solutions_array_1)
+#     plt.title("Chosen step: "+str(step_array_1[i]))
+#     plt.grid(visible=True)
+#     plt.scatter(starting_point_array_1, solutions_array_1, marker='o')
+#     plt.subplots_adjust(hspace=0.7)
+#
+#
+# plt.xlabel('Randomized points')
+# plt.ylabel('Calculated function minimum')
+# plt.show()
 
-for i in range(len(step_array_1)):
-    solutions_array_1 = list(
-        map(lambda starting_point: calculate_function_minimum(function1, grad1, starting_point, step_array_1[i], 0.9, 15, 0.001),
-            starting_point_array_1))
-
-    df = pd.DataFrame(dict(
-        x=starting_point_array_1,
-        y=solutions_array_1
-    ))
-
-
-    plt.subplot(4,1,i+1)
-    plt.plot(starting_point_array_1, solutions_array_1)
-    plt.title("Chosen step: "+str(step_array_1[i]))
-    plt.grid(visible=True)
-    plt.scatter(starting_point_array_1, solutions_array_1, marker='o')
-    plt.subplots_adjust(hspace=0.7)
-
-
-plt.xlabel('Randomized points')
-plt.ylabel('Calculated function minimum')
-plt.show()
-
+function_params = calculate_function_minimum_from_input()
+calculate_function_minimum(function_params)
 
 print('')
 #===========================function2================================
